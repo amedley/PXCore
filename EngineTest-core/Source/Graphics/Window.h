c081000710b78620c91753b8216b524b1161f57e
Kudoku96 <gundlachandrew@gmail.com>
2017-03-26 02:34:20 -0400

    Added input for keyboard and mouse.

--- EngineTest-core/Source/Graphics/Window.h
+++ EngineTest-core/Source/Graphics/Window.h
@@ -10,28 +10,52 @@ namespace EngineTest
 {
 	namespace Graphics
 	{
+
+#define MAX_KEYS				1024
+#define MAX_MOUSE_BUTTONS		32
+
 		class Window
 		{
 		private:
-			const char *m_Title;
+			const char* m_Title;
 			int m_Width, m_Height;
-			GLFWwindow *m_Window;
+			GLFWwindow* m_Window;
+
+			bool m_KeysDown[MAX_KEYS];
+			bool m_KeysPressed[MAX_KEYS];
+			bool m_KeysReleased[MAX_KEYS];
+			bool m_MouseButtonsDown[MAX_MOUSE_BUTTONS];
+			bool m_MouseButtonsPressed[MAX_MOUSE_BUTTONS];
+			bool m_MouseButtonsReleased[MAX_MOUSE_BUTTONS];
+			double m_MouseX, m_MouseY;
 		public:
-			Window(const char *title, int width, int height);
+			Window(const char* title, int width, int height);
 			~Window();
-			void clear() const;
-			void update();
-			bool closed() const;
+			void Clear() const;
+			void OnUpdate();
+			bool IsClosed() const;
+			bool IsKeyDown(unsigned int key) const;
+			bool IsKeyPressed(unsigned int key) const;
+			bool IsKeyReleased(unsigned int key) const;
+			bool IsMouseButtonDown(unsigned int button) const;
+			bool IsMouseButtonPressed(unsigned int button) const;
+			bool IsMouseButtonReleased(unsigned int button) const;
 
-			void setWidth(int width);
-			void setHeight(int height);
+			void SetWidth(int width);
+			void SetHeight(int height);
 
-			inline int getWidth() const { return m_Width; }
-			inline int getHeight() const { return m_Height; }
+			inline int GetWidth() const { return m_Width; }
+			inline int GetHeight() const { return m_Height; }
+			inline double GetMouseX() const { return m_MouseX; }
+			inline double GetMouseY() const { return m_MouseY; }
 		private:
-			bool init();
+			bool Init();
+
+			static void resize_callback(GLFWwindow* window, int width, int height);
+			static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+			static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+			static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
 		};
-		static Window *mainWindow;
 	}
 }
 
